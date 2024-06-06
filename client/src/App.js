ccf919d2194274fcd4ce283ccb8dcdc266a263a1
PythonShinobi <pythonshinobi@gmail.com>
2024-06-06 13:55:03 +0300

    Add the Navbar component to App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// client/src/App.js
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Navbar from "./Navbar/Navbar";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+        </Router>
+    );
+};
+
+export default App;
