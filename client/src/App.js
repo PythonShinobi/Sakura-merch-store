edd93e33942224501520b09762f568b01a4f47b1
PythonShinobi <pythonshinobi@gmail.com>
2024-06-07 13:01:26 +0300

    Add Tshirts componet to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero-Section/Hero";
+import TShirts from "./t-shirts/t-shirts";
 
 function App() {
     return (
         <Router>
             <Navbar />
             <Hero />
+            <TShirts />
         </Router>
     );
 };
