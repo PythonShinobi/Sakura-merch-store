dbd703c45536db16724f93e1b2e4f226e7fe15df
PythonShinobi <pythonshinobi@gmail.com>
2024-06-08 09:32:26 +0300

    Add Shoe component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero-Section/Hero";
 import TShirts from "./t-shirts/t-shirts";
+import Shoes from "./shoes/shoes";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Navbar />
             <Hero />
             <TShirts />
+            <Shoes />
         </Router>
     );
 };
